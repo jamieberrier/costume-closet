17792b7958531abb71bbadb18ece225cad25b5b6
jamieberrier <berrier.jamie@gmail.com>
2020-02-19 12:07:46 -0800

    add controller actions

--- app/controllers/dance_studios_controller.rb
+++ app/controllers/dance_studios_controller.rb
@@ -1,2 +1,26 @@
 class DanceStudiosController < ApplicationController
+  before_action :require_logged_in, only: [:show]
+  
+  def new
+    @dance_studio = DanceStudio.new
+  end
+
+  def create
+    @dance_studio = DanceStudio.new(dance_studio_params)
+
+    return redirect_to new_dance_studio_path unless @dance_studio.save
+
+    session[:user_id] = @dance_studio.id
+
+    redirect_to dance_studio_path(@dance_studio.id)
+  end
+
+  def show
+  end
+
+  private
+
+  def dance_studio_params
+    params.require(:dance_studio).permit(:email, :password, :studio_name, :owner_name)
+  end
 end
