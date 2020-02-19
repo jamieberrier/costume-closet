72083ec092152c0f6759981b5a41ecc460c6c78e
jamieberrier <berrier.jamie@gmail.com>
2020-02-19 12:01:09 -0800

    add root route => home page, register routes, and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  root 'application#home'
+
   resources :costume_assignments
   resources :costumes
   resources :dancers
   resources :dance_studios
+
+  get '/register' => 'dance_studios#new'
+  post '/register' => 'dance_studios#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
