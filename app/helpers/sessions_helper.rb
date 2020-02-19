961c5f07171612d465ddef293b1d767f8893285d
jamieberrier <berrier.jamie@gmail.com>
2020-02-19 11:43:49 -0800

    add sessions helper methods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+  def current_user
+    @dance_studio ||= DanceStudio.find_by(id: session[:user_id])
+  end
+  
+  def logged_in?
+    !!current_user
+  end
+
+  def require_logged_in
+    return redirect_to root_path unless logged_in?
+  end
 end
