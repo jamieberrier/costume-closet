ecd464bfb6b02fdc318a26a8c1fa97fe99cb1053
jamieberrier <berrier.jamie@gmail.com>
2020-02-19 12:19:13 -0800

    add controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,30 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @dance_studio = DanceStudio.find_by(email: params[:dance_studio][:email])
+    # try is an ActiveSupport method
+    # object.try(:some_method) means
+    # if object != nil then object.some_method else nil end.
+    authenticated = @dance_studio.try(:authenticate, params[:dance_studio][:password])
+
+    if @dance_studio.nil?
+      redirect_to login_path, danger: 'Enter Email'
+    else
+      # Users should not be able to log in if they enter an incorrect password.
+      # @dance_studio.authenticated(params[:dance_studio][:password])
+      if authenticated
+        session[:user_id] = @dance_studio.id
+        redirect_to dance_studio_path(@dance_studio.id)
+      else
+        redirect_to login_path, info: 'Invalid Password'
+      end
+    end
+  end
+
+  def destroy
+    session.destroy
+    redirect_to root_path
+  end
 end
