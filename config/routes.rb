969079d122001c2cb281ab4baa64751b8b3511a8
jamieberrier <berrier.jamie@gmail.com>
2020-02-20 12:53:21 -0800

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,25 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # You can have the root of your site routed with "root"
+  #   root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of Custom Route:
+  #   get "students/:id/activate", to: "students#activate", as: "activate_student"
+
   root 'application#home'
 
   resources :costume_assignments
   resources :costumes
   resources :dancers
-  resources :dance_studios, only: [:index, :edit, :show, :update, :destroy]
+  resources :dance_studios, only: [:create, :index, :edit, :show, :update, :destroy]
 
   get '/register' => 'dance_studios#new'
-  post '/register' => 'dance_studios#create'
-
+  
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
