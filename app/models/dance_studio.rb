3aaf60dfd3da3276b95d7c7a85fc1ca24579f2ff
jamieberrier <berrier.jamie@gmail.com>
2020-02-20 12:57:28 -0800

    add validations for email, password confirmation, studio name, and owner name

--- app/models/dance_studio.rb
+++ app/models/dance_studio.rb
@@ -1,5 +1,9 @@
 class DanceStudio < ApplicationRecord
   has_many :dancers
   has_many :costumes
+
   has_secure_password
+  validates :password, confirmation: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :studio_name, :owner_name, :password_confirmation, presence: true
 end
