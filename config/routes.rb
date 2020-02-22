12cc897d627d474ffc1d14e6142084f96c93f59a
jamieberrier <berrier.jamie@gmail.com>
2020-02-21 18:48:52 -0800

    add route to finishing signing up via google

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :dance_studios, only: [:create, :index, :edit, :show, :update, :destroy]
 
   get '/register' => 'dance_studios#new'
+  get '/register/google' => 'dance_studios#googleAuth', as: 'google_register'
   
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
