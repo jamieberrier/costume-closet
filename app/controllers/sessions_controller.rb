cc6771974bd08b31714b72916353255ed399c539
jamieberrier <berrier.jamie@gmail.com>
2020-02-21 18:55:17 -0800

    add log in helper method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,8 +15,8 @@ class SessionsController < ApplicationController
         redirect_to login_path, danger: 'Incorrect Email'
       else # not be able to log in if password is incorrect
         if authenticated
-          session[:user_id] = @dance_studio.id
-          redirect_to dance_studio_path(@dance_studio), success: 'Successfully logged in!'
+          log_in(@dance_studio)
+          #redirect_to dance_studio_path(@dance_studio), success: 'Successfully logged in!'
         else
           redirect_to login_path, danger: 'Invalid Password'
         end
@@ -27,20 +27,23 @@ class SessionsController < ApplicationController
   def googleAuth
     # Get access tokens from the google server
     access_token = request.env["omniauth.auth"]
-    dance_studio = DanceStudio.from_omniauth(access_token)
-    # log_in(dance_studio)
+    @dance_studio = DanceStudio.from_omniauth(access_token)
     # Access_token is used to authenticate request made from the rails application to the google server
     # Only save the token if you are planning to use Google APIs (Calendar, Spreadsheet.. etc)
-    # dance_studio.google_token = access_token.credentials.token
-
+    @dance_studio.google_token = access_token.credentials.token
     # Refresh_token to request new access_token
     # Note: Refresh_token is only sent once during the first request
     refresh_token = access_token.credentials.refresh_token
-    dance_studio.google_refresh_token = refresh_token if refresh_token.present?
-    dance_studio.save
-    binding.pry
-    # redirect_to root_path
-    redirect_to dance_studio_path(dance_studio), success: 'Successfully logged in!'
+    @dance_studio.google_refresh_token = refresh_token if refresh_token.present?
+    # dance_studio.save
+    if @dance_studio.id.nil?
+      session[:dance_studio] = @dance_studio
+      redirect_to google_register_path
+    else
+      @dance_studio.save
+      log_in(@dance_studio)
+      #redirect_to dance_studio_path(@dance_studio), success: 'Successfully logged in!'
+    end
   end
 
   def destroy
@@ -48,4 +51,8 @@ class SessionsController < ApplicationController
     redirect_to root_path, success: 'Successfully logged out!'
   end
 
+  def log_in(dance_studio)
+    session[:user_id] = dance_studio.id
+    redirect_to dance_studio_path(dance_studio), success: 'Successfully logged in!'
+  end
 end
