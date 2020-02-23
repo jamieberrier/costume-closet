fccfbeb4009f725d17ea6ae2c482ab5a61c702ca
jamieberrier <berrier.jamie@gmail.com>
2020-02-23 10:19:14 -0800

    create view, form, and controller actions to create a new costume

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,2 +1,26 @@
 class CostumesController < ApplicationController
+  def new
+    @costume = Costume.new(dance_studio_id: params[:dance_studio_id])
+  end
+
+  def create
+    @costume = Costume.new(costume_params)
+
+    if @costume.save
+      redirect_to dance_studio_costume_path(@costume, dance_studio_id: costume_params[:dance_studio_id])
+    else
+      render :new
+    end
+  end
+
+  def show
+    
+  end
+
+  private
+
+  def costume_params
+    params.require(:costume).permit(:top_description, :bottoms_description, :onepiece_description, :picture, :hair_accessory, :dance_studio_id)
+  end
+
 end
