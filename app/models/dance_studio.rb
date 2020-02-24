1028b0ee8115e993b55f7f8430e0ad6db8e0d6c1
jamieberrier <berrier.jamie@gmail.com>
2020-02-24 12:37:01 -0800

    update spacing

--- app/models/dance_studio.rb
+++ app/models/dance_studio.rb
@@ -3,6 +3,7 @@ class DanceStudio < ApplicationRecord
   has_many :costumes
 
   has_secure_password
+
   validates :password, confirmation: { case_sensitive: false }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :studio_name, :owner_name, :password_confirmation, presence: true
