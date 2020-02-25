a44b45e34436a8af8971bbdac7178c33034c75e9
jamieberrier <berrier.jamie@gmail.com>
2020-02-25 13:42:43 -0800

    update routes for registering as a dance studio or a dancer and dancer routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,22 +10,26 @@ Rails.application.routes.draw do
   # Example of Custom Route:
   #   get "students/:id/activate" => "students#activate", as: "activate_student"
 
+  # root route
   root 'application#home'
 
-  resources :dance_studios do
+  resources :dance_studios, only: %i[create show edit update destroy] do
     resources :costumes
-    resources :dancers
+    resources :dancers, only: %i[show edit update destroy]
     resources :costume_assignments
   end
 
-  # Routes for signing up
-  get '/register' => 'dance_studios#new'
-  get '/register/google' => 'dance_studios#googleAuth', as: 'google_register'
+  resources :dancers, only: %i[new create]
+
+  # Routes for signing up dance studio
+  get '/register/dance_studio' => 'dance_studios#new'
+  get '/register/dance_studio/google' => 'dance_studios#googleAuth', as: 'dance_studio_google_register'
+  # Routes for signing up dancer via Google
+  get '/register/dancer/google' => 'dancers#googleAuth', as: 'dancer_google_register'
   # Routes for logging in
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
-
   # Routes for Google authentication
   get 'auth/:provider/callback', to: 'sessions#googleAuth'
   get 'auth/failure', to: redirect('application#home')
