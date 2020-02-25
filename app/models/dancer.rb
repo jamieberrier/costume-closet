318dc38485d90dd2edea62369f4f32d1770e1cb3
jamieberrier <berrier.jamie@gmail.com>
2020-02-25 13:47:36 -0800

    add validations

--- app/models/dancer.rb
+++ app/models/dancer.rb
@@ -2,4 +2,17 @@ class Dancer < ApplicationRecord
   belongs_to :dance_studio
   has_many :costume_assignments
   has_many :costumes, through: :costume_assignments
+
+  has_secure_password
+
+  validates :password, confirmation: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, :last_name, :password_confirmation, presence: true
+
+  def self.from_omniauth(auth)
+    # Creates a new user only if it doesn't exist
+    where(email: auth.info.email).first_or_initialize do |user|
+      user.email = auth.info.email
+    end
+  end
 end
