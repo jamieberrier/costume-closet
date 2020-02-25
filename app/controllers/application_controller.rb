720b92d262b54104250aeee1add28dd58889356c
jamieberrier <berrier.jamie@gmail.com>
2020-02-24 18:13:41 -0800

    add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   add_flash_types :success, :danger, :warning, :info
-  
+
   include SessionsHelper
+
+  def home
+    redirect_to dance_studio_path(current_user), info: "You are already logged in" unless !logged_in?
+  end
 end
