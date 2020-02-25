72acceefba9a938a303fe0d8a46e2494b32800b8
jamieberrier <berrier.jamie@gmail.com>
2020-02-25 13:49:12 -0800

    update methods to account for dancer and dance_studio

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  before_action :require_logged_in, only: [:destroy]
+  before_action :require_logged_in!, only: [:destroy]
 
   def new
     redirect_if_logged_in
@@ -7,13 +7,33 @@ class SessionsController < ApplicationController
 
   def create
     # if email is blank, redirect to /login
-    if params[:dance_studio][:email].blank?
+    if params[:user][:email].blank?
       redirect_to login_path, warning: 'Enter Email'
-    else # find dance studio & try to authenticate password
-      @dance_studio = DanceStudio.find_by(email: params[:dance_studio][:email])
-      # try is an ActiveSupport method: object.try(:some_method) means if object != nil then object.some_method else nil end.
-      authenticated = @dance_studio.try(:authenticate, params[:dance_studio][:password])
-
+    else # find dance studio or dancer
+      @dance_studio = DanceStudio.find_by(email: params[:user][:email])
+      @dancer = Dancer.find_by(email: params[:user][:email])
+      # try to authenticate password
+        # try is an ActiveSupport method: object.try(:some_method) means if object != nil then object.some_method else nil end.
+      if @dance_studio
+        authenticated = @dance_studio.try(:authenticate, params[:user][:password])
+        if authenticated
+          log_in(@dance_studio, 'Successfully Logged In!')
+        else
+          redirect_to login_path, danger: 'Invalid Password'
+        end
+      elsif @dancer
+        authenticated = @dancer.try(:authenticate, params[:user][:password])
+        if authenticated
+          log_in(@dancer, 'Successfully Logged In!')
+        else
+          redirect_to login_path, danger: 'Invalid Password'
+        end
+      else # no dance studio/dancer found, redirect to /login
+        redirect_to login_path, danger: 'Incorrect Email'
+      end
+    end
+  end
+=begin
       if @dance_studio.nil? # if no dance studio found, redirect to /login
         redirect_to login_path, danger: 'Incorrect Email'
       else # check for correct password & log in
@@ -23,20 +43,21 @@ class SessionsController < ApplicationController
           redirect_to login_path, danger: 'Invalid Password'
         end
       end
-    end
-  end
+=end
 
   def googleAuth
     # Get access tokens from the google server
     # Access_token is used to authenticate request made from the rails application to the google server
     access_token = request.env["omniauth.auth"]
+    # control for dancer vs owner
+    # binding.pry
     @dance_studio = DanceStudio.from_omniauth(access_token)
     if @dance_studio.id
       @dance_studio.save
       log_in(@dance_studio, 'Successfully Logged In!')
     else
       session[:dance_studio] = @dance_studio
-      redirect_to google_register_path
+      redirect_to dance_studio_google_register_path
     end
   end
 
