adfb289b7f57231eaab5393bbb6e4c1b6b28ee78
jamieberrier <berrier.jamie@gmail.com>
2020-02-25 13:40:49 -0800

    add before action is_owner?

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,5 +1,5 @@
 class CostumesController < ApplicationController
-  before_action :require_logged_in!
+  before_action :require_logged_in!, :is_owner?
 
   def new
     @costume = Costume.new(dance_studio_id: params[:dance_studio_id])
