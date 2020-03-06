551b59fc66ae0cf93917527b1445f7963ba5dd17
jamieberrier <berrier.jamie@gmail.com>
2020-03-06 13:17:51 -0800

    add index action

--- app/controllers/costume_assignments_controller.rb
+++ app/controllers/costume_assignments_controller.rb
@@ -1,2 +1,10 @@
 class CostumeAssignmentsController < ApplicationController
+  def index
+    if params.include?(:dancer_id)
+      @dancer = Dancer.find(params[:dancer_id])
+      @costume_assignments = CostumeAssignment.where(dancer_id: @dancer.id).order(:dance_season, :genre, :song_name)
+    else
+      @costume_assignments = CostumeAssignment.all.order(:dance_season, :genre, :song_name)
+    end
+  end
 end
