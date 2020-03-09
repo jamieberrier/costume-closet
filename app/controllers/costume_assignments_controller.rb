435d3a6b343180fb23aafff9e557b988cf56283c
jamieberrier <berrier.jamie@gmail.com>
2020-03-09 15:30:49 -0700

    add show and current_assignments actions

--- app/controllers/costume_assignments_controller.rb
+++ app/controllers/costume_assignments_controller.rb
@@ -11,4 +11,23 @@ class CostumeAssignmentsController < ApplicationController
       @costume_assignments = CostumeAssignment.all.order(:dance_season, :genre, :song_name)
     end
   end
+
+  def show
+    binding.pry
+    @info = CostumeAssignment.find_by(costume_id: params[:id])
+    @costume_assignments = CostumeAssignment.where(costume_id: params[:id])
+    @dancers = []
+
+    @costume_assignments.each do |assignment|
+      @dancers << Dancer.find(assignment.dancer_id)
+    end
+  end
+
+  def current_assignments
+    if current_user.class.name == 'Dancer'
+      @costume_assignments = CostumeAssignment.current_costumes(current_user)
+    else
+      @costume_assignments = CostumeAssignment.current_studio_costumes(current_user)
+    end
+  end
 end
