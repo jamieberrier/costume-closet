6244b5003a2827fee4e1d1240a8bed2cd06aa32b
jamieberrier <berrier.jamie@gmail.com>
2020-03-09 11:02:09 -0700

    add edit and update actions

--- app/controllers/dance_studios_controller.rb
+++ app/controllers/dance_studios_controller.rb
@@ -12,6 +12,16 @@ class DanceStudiosController < ApplicationController
 
   end
 
+  def edit
+    @dance_studio = DanceStudio.find(params[:id])
+  end
+
+  def update
+    @dance_studio = DanceStudio.find(params[:id])
+    @dance_studio.update(dance_studio_params)
+    redirect_to dance_studio_path(@dance_studio), success: 'Account Info Updated!'
+  end
+
   private
 
   def dance_studio_params
