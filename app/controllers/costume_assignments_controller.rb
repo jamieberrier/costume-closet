e7e4fe1ac1618e76d8b0172b68db48341258fbc6
jamieberrier <berrier.jamie@gmail.com>
2020-03-09 10:51:08 -0700

    add before_action and update index action to show a dance studio the costume assignments of one of its dancers

--- app/controllers/costume_assignments_controller.rb
+++ app/controllers/costume_assignments_controller.rb
@@ -1,8 +1,12 @@
 class CostumeAssignmentsController < ApplicationController
+  before_action :require_logged_in!
+
   def index
-    if params.include?(:dancer_id)
+    if current_user.class.name == 'Dancer'
+      @costume_assignments = CostumeAssignment.where(dancer_id: current_user.id).order(:dance_season, :genre, :song_name)
+    elsif request.env['HTTP_REFERER'].include?('dancers')
       @dancer = Dancer.find(params[:dancer_id])
-      @costume_assignments = CostumeAssignment.where(dancer_id: @dancer.id).order(:dance_season, :genre, :song_name)
+      @costume_assignments = CostumeAssignment.where(dancer_id: @dancer).order(:dance_season, :genre, :song_name)
     else
       @costume_assignments = CostumeAssignment.all.order(:dance_season, :genre, :song_name)
     end
