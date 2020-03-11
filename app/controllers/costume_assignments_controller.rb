becb3f9529d373be78ca63858efb3717ad30ac87
jamieberrier <berrier.jamie@gmail.com>
2020-03-11 10:28:49 -0700

    update show action

--- app/controllers/costume_assignments_controller.rb
+++ app/controllers/costume_assignments_controller.rb
@@ -5,26 +5,21 @@ class CostumeAssignmentsController < ApplicationController
     if current_user.class.name == 'Dancer'
       @costume_assignments = CostumeAssignment.where(dancer_id: current_user.id).order(:dance_season, :genre, :song_name)
     elsif request.env['HTTP_REFERER'].include?('dancers')
-      @dancer = Dancer.find(params[:dancer_id])
+      @dancer = Dancer.find(params[:id])
       @costume_assignments = CostumeAssignment.where(dancer_id: @dancer).order(:dance_season, :genre, :song_name)
+    elsif params[:costume_id]
+      @costume_assignments = CostumeAssignment.where(costume_id: params[:costume_id])
     else
       @costume_assignments = CostumeAssignment.all.order(:dance_season, :genre, :song_name)
     end
   end
 
   def show
-    binding.pry
-    @info = CostumeAssignment.find_by(costume_id: params[:id])
-    @costume_assignments = CostumeAssignment.where(costume_id: params[:id])
-    @dancers = []
-
-    @costume_assignments.each do |assignment|
-      @dancers << Dancer.find(assignment.dancer_id)
-    end
+    @costume_assignment = CostumeAssignment.find_by(id: params[:id])
   end
 
   def current_assignments
-    if current_user.class.name == 'Dancer'
+    if dancer?
       @costume_assignments = CostumeAssignment.current_costumes(current_user)
     else
       @costume_assignments = CostumeAssignment.current_studio_costumes(current_user)
