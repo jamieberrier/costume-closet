0ba9a96ebe9e5e66829e4714ef5f79cb1b5b194a
jamieberrier <berrier.jamie@gmail.com>
2020-03-16 14:26:17 -0700

    update logic for different index views

--- app/controllers/costume_assignments_controller.rb
+++ app/controllers/costume_assignments_controller.rb
@@ -2,15 +2,18 @@ class CostumeAssignmentsController < ApplicationController
   before_action :require_logged_in!
 
   def index
-    if current_user.class.name == 'Dancer'
-      @costume_assignments = CostumeAssignment.where(dancer_id: current_user.id).order(:dance_season, :genre, :song_name)
-    elsif request.env['HTTP_REFERER'].include?('dancers')
+    if owner?
+      if params[:costume_id] # owner viewing a costume's assignments
+        @costume_assignments = CostumeAssignment.where(costume_id: params[:costume_id])
+      elsif params[:id] # owner viewing a dancer's costume assignments
+        @dancer = Dancer.find(params[:id])
+        @costume_assignments = CostumeAssignment.where(dancer_id: @dancer).order(:dance_season, :genre, :song_name)
+      else # owner viewing its dance studio's costume assignments
+        @costume_assignments = current_user.costume_assignments.order(dance_season: :desc)
+      end
+    else # dancer viewing his/her costume assignments
       @dancer = Dancer.find(params[:id])
       @costume_assignments = CostumeAssignment.where(dancer_id: @dancer).order(:dance_season, :genre, :song_name)
-    elsif params[:costume_id]
-      @costume_assignments = CostumeAssignment.where(costume_id: params[:costume_id])
-    else
-      @costume_assignments = CostumeAssignment.all.order(:dance_season, :genre, :song_name)
     end
   end
 
