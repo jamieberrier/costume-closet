046e34106b640f85cc0c1eaa7103c65e65ce3f75
jamieberrier <berrier.jamie@gmail.com>
2020-03-19 14:32:16 -0700

    add new and create actions

--- app/controllers/costume_assignments_controller.rb
+++ app/controllers/costume_assignments_controller.rb
@@ -1,6 +1,16 @@
 class CostumeAssignmentsController < ApplicationController
   before_action :require_logged_in!
 
+  def new
+    @assignment = CostumeAssignment.new
+  end
+
+  def create
+    @assignment = CostumeAssignment.new(assignment_params)
+    flash.now[:danger] = "Creation failure: #{@assignment.errors.full_messages.to_sentence}"
+    render 'costume_assignments/new' unless @assignment.save
+  end
+
   def index
     if owner?
       if params[:costume_id] # owner viewing a costume's assignments
@@ -19,16 +29,26 @@ class CostumeAssignmentsController < ApplicationController
   end
 
   def show
-    @costume_assignment = CostumeAssignment.find_by(id: params[:id])
+    find_assignment
   end
 
   def current_assignments
     if dancer?
-      @costume_assignments = CostumeAssignment.current_costumes(current_user)
+      @assignments = CostumeAssignment.current_costumes(current_user)
     else
-      @costume_assignments = CostumeAssignment.current_studio_costumes(current_user)
+      @assignments = CostumeAssignment.current_studio_costumes(current_user)
       # can I deal wiht this in view instead?
-      @costumes = Costume.find_by_assignment(@costume_assignments)
+      @costumes = Costume.find_by_assignment(@assignments)
     end
+  end
+
+  private
+
+  def assignment_params
+    params.require(:costume_assignment).permit(:dancer_id, :costume_id, :costume_condition, :costume_size, :song_name, :genre, :dance_season, :shoe, :tight)
+  end
+
+  def find_assignment
+    @costume_assignment = CostumeAssignment.find(params[:id])
   end
 end
