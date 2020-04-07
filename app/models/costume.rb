53adddde916a2be2f2d9409d5e2a4c9f300ccb90
jamieberrier <berrier.jamie@gmail.com>
2020-04-07 15:32:24 -0700

    add method to concatenate twopiece description and a custom writer method for costume assignments attributes

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,6 +1,8 @@
 class Costume < ApplicationRecord
   belongs_to :dance_studio
+  # adds methods, ie: costume_assignments, costume_assignments<<, costume_assignments.delete, costume_assignments_ids=ids
   has_many :costume_assignments
+  # adds methods, ie: dancer_ids=ids
   has_many :dancers, through: :costume_assignments
 
   validates :onepiece_description, presence: { message: "must enter a onepiece description if one piece costume OR a top & bottoms description if two piece costume" }, if: :onepiece_costume?
@@ -14,11 +16,29 @@ class Costume < ApplicationRecord
     onepiece_description.blank?
   end
 
+  def twopiece_description
+    top_description + ' with ' + bottoms_description
+  end
+
   def self.find_by_assignment(assignments)
     costumes = []
     assignments.each do |assignment|
       costumes << Costume.find(assignment.costume_id)
     end
     costumes.uniq
+  end
+
+  def costume_assignments_attributes=(assignments_hashes)
+    # get shared data and remove from hash
+    shared = assignments_hashes.shift
+    shared_attributes = shared.pop
+    # delete where dancer_id == 0
+    assignments_hashes.delete_if { |key, value| value[:dancer_id] == '0' }
+
+    assignments_hashes.each do |i, unique_attributes|
+      # combine unique dancer data with shared data
+      combined_attributes = unique_attributes.merge(shared_attributes)
+      self.costume_assignments.build(combined_attributes)
+    end
   end
 end
