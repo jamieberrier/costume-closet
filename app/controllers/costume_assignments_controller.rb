6e163808fe173af777478e19c9fac8556b94f11a
jamieberrier <berrier.jamie@gmail.com>
2020-04-14 12:03:16 -0700

    comment out unused actions

--- app/controllers/costume_assignments_controller.rb
+++ app/controllers/costume_assignments_controller.rb
@@ -1,19 +1,6 @@
 class CostumeAssignmentsController < ApplicationController
   before_action :require_logged_in!
-
-  def new
-    @assignment = CostumeAssignment.new
-  end
-
-  def create
-    @assignment = CostumeAssignment.new(assignment_params)
-
-    flash.now[:danger] = "Creation failure: #{@assignment.errors.full_messages.to_sentence}"
-    render 'costume_assignments/new' unless @assignment.save
-
-    redirect_to costume_assignment_path(@assignment)
-  end
-
+  # break out into other controllers?
   def index
     if owner?
       if params[:costume_id] # owner viewing a costume's assignments
@@ -30,36 +17,39 @@ class CostumeAssignmentsController < ApplicationController
       @costume_assignments = CostumeAssignment.where(dancer_id: @dancer).order(:dance_season, :genre, :song_name)
     end
   end
-  # am I using this?
-  def show
-    binding.pry
-    find_assignment
-  end
-  # assign costume actions
+
+  # assign costume actions...new?
   def assign_costume
 
   end
-
+  # create?
   def assign
 
   end
   
 =begin
-  # copied and split in dancer and dancestudio controllers
-  def current_assignments
-    if dancer?
-      @assignments = CostumeAssignment.current_costumes(current_user)
-    else
-      @assignments = CostumeAssignment.current_studio_costumes(current_user)
-      # can I deal wiht this in view instead?
-      @costumes = Costume.find_by_assignment(@assignments)
-    end
+  def new
+    @assignment = CostumeAssignment.new
   end
-=end
+
+  def create
+    @assignment = CostumeAssignment.new(assignment_params)
+
+    flash.now[:danger] = "Creation failure: #{@assignment.errors.full_messages.to_sentence}"
+    render 'costume_assignments/new' unless @assignment.save
+
+    redirect_to costume_assignment_path(@assignment)
+  end
+  # am I using this?
+  def show
+    binding.pry
+    find_assignment
+  end
 
   private
 
   def assignment_params
     params.require(:costume_assignment).permit(:dancer_id, :costume_id, :costume_condition, :costume_size, :song_name, :genre, :dance_season, :shoe, :tight)
   end
+=end
 end
