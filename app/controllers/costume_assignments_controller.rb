152ccc6481f4b6a64e35998bb9d5a1ae97809243
jamieberrier <berrier.jamie@gmail.com>
2020-04-17 15:58:20 -0700

    create dancer_assignments action and view and move code

--- app/controllers/costume_assignments_controller.rb
+++ app/controllers/costume_assignments_controller.rb
@@ -5,32 +5,16 @@ class CostumeAssignmentsController < ApplicationController
   # owner viewing all of its dance studio's costume assignments
   def index
     @costume_assignments = current_user.costume_assignments.order(dance_season: :desc)
-    @back_page = dance_studio_path(current_user)
-    #if owner?
-      #if params[:costume_id] # owner viewing a costume's assignments
-        #find_costume
-        #@costume_assignments = CostumeAssignment.where(costume_id: @costume)
-        #@back_page = dance_studio_costume_assignments_path(current_user)
-      #elsif params[:id] # owner viewing a dancer's costume assignments
-        #find_dancer
-        #@costume_assignments = CostumeAssignment.where(dancer_id: @dancer).order(:dance_season, :genre, :song_name)
-      #else # owner viewing all of its dance studio's costume assignments
-      #@costume_assignments = current_user.costume_assignments.order(dance_season: :desc)
-      #@back_page = dance_studio_path(current_user)
-      #end
-    #else # dancer viewing all of his/her costume assignments
-      #find_dancer
-      #@costume_assignments = CostumeAssignment.where(dancer_id: @dancer).order(:dance_season, :genre, :song_name)
-    #end
   end
+
   # owner viewing a costume's assignments -- params[:costume_id]
   def costume_assignments
     find_costume
     @costume_assignments = CostumeAssignment.where(costume_id: @costume)
   end
+
   # dancer viewing all of his/her costume assignments / owner viewing a dancer's costume assignments -- params[:id]
   def dancer_assignments
-    binding.pry
     find_dancer
     @costume_assignments = CostumeAssignment.where(dancer_id: @dancer).order(:dance_season, :genre, :song_name)
   end
