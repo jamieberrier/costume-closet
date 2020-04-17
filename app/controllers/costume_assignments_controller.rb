b6b3ff9e48e986c71a4112af5ed1dd61d7ced37b
jamieberrier <berrier.jamie@gmail.com>
2020-04-17 15:41:33 -0700

    break out if statement in index into 2 actions, costume_assignments and dancer_assignments

--- app/controllers/costume_assignments_controller.rb
+++ app/controllers/costume_assignments_controller.rb
@@ -1,21 +1,38 @@
 class CostumeAssignmentsController < ApplicationController
   before_action :require_logged_in!
-  # break out into other controllers?
+  before_action :owner?, only: %i[index costume_assignments]
+
+  # owner viewing all of its dance studio's costume assignments
   def index
-    if owner?
-      if params[:costume_id] # owner viewing a costume's assignments
-        find_costume
-        @costume_assignments = CostumeAssignment.where(costume_id: @costume)
-      elsif params[:id] # owner viewing a dancer's costume assignments
-        find_dancer
-        @costume_assignments = CostumeAssignment.where(dancer_id: @dancer).order(:dance_season, :genre, :song_name)
-      else # owner viewing its dance studio's costume assignments
-        @costume_assignments = current_user.costume_assignments.order(dance_season: :desc)
-      end
-    else # dancer viewing his/her costume assignments
-      find_dancer
-      @costume_assignments = CostumeAssignment.where(dancer_id: @dancer).order(:dance_season, :genre, :song_name)
-    end
+    @costume_assignments = current_user.costume_assignments.order(dance_season: :desc)
+    @back_page = dance_studio_path(current_user)
+    #if owner?
+      #if params[:costume_id] # owner viewing a costume's assignments
+        #find_costume
+        #@costume_assignments = CostumeAssignment.where(costume_id: @costume)
+        #@back_page = dance_studio_costume_assignments_path(current_user)
+      #elsif params[:id] # owner viewing a dancer's costume assignments
+        #find_dancer
+        #@costume_assignments = CostumeAssignment.where(dancer_id: @dancer).order(:dance_season, :genre, :song_name)
+      #else # owner viewing all of its dance studio's costume assignments
+      #@costume_assignments = current_user.costume_assignments.order(dance_season: :desc)
+      #@back_page = dance_studio_path(current_user)
+      #end
+    #else # dancer viewing all of his/her costume assignments
+      #find_dancer
+      #@costume_assignments = CostumeAssignment.where(dancer_id: @dancer).order(:dance_season, :genre, :song_name)
+    #end
+  end
+  # owner viewing a costume's assignments -- params[:costume_id]
+  def costume_assignments
+    find_costume
+    @costume_assignments = CostumeAssignment.where(costume_id: @costume)
+  end
+  # dancer viewing all of his/her costume assignments / owner viewing a dancer's costume assignments -- params[:id]
+  def dancer_assignments
+    binding.pry
+    find_dancer
+    @costume_assignments = CostumeAssignment.where(dancer_id: @dancer).order(:dance_season, :genre, :song_name)
   end
 
   # assign costume actions...new?
@@ -26,6 +43,7 @@ class CostumeAssignmentsController < ApplicationController
   def assign
 
   end
+end
   
 =begin
   def new
@@ -52,4 +70,3 @@ class CostumeAssignmentsController < ApplicationController
     params.require(:costume_assignment).permit(:dancer_id, :costume_id, :costume_condition, :costume_size, :song_name, :genre, :dance_season, :shoe, :tight)
   end
 =end
-end
