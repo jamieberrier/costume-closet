1e6bb1f3523a2ea4f22890e6370843d16b59c925
jamieberrier <berrier.jamie@gmail.com>
2020-04-21 11:49:16 -0700

    add helper method that returns true if a costume is currently unassigned

--- app/helpers/costume_assignments_helper.rb
+++ app/helpers/costume_assignments_helper.rb
@@ -2,4 +2,9 @@ module CostumeAssignmentsHelper
   def find_assignment
     @costume_assignment = CostumeAssignment.find(params[:id])
   end
+
+  def unassigned?
+    # return true if @costume has no rows w/ 2020 dance season in CostumeAssignments table
+    current_user.unassigned_studio_costumes.include?(@costume)
+  end
 end
