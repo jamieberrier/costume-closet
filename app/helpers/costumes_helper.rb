3a74da5377d3a2929c0e41489d174b5750bb1f67
jamieberrier <berrier.jamie@gmail.com>
2020-04-21 12:22:06 -0700

    add @back_page for the show page's back button

--- app/helpers/costumes_helper.rb
+++ app/helpers/costumes_helper.rb
@@ -4,6 +4,8 @@ module CostumesHelper
   end
 
   def redirect_to_costume_path(message)
-    redirect_to costume_path(@costume), success: message
+    # path for Back button on costume show page
+    @back_page = dance_studio_costumes_path(current_user)
+    redirect_to costume_path(@costume, back_page: @back_page), success: message
   end
 end
