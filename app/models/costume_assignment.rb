7c65b0f67116fc075f026b9ccf2156ff9529e217
jamieberrier <berrier.jamie@gmail.com>
2020-04-22 16:35:53 -0700

    delete validation for costume_id

--- app/models/costume_assignment.rb
+++ app/models/costume_assignment.rb
@@ -2,7 +2,7 @@ class CostumeAssignment < ApplicationRecord
   belongs_to :dancer
   belongs_to :costume
   # costume_size, genre, shoe, tight, costume_condition
-  validates :dancer_id, :costume_id, :dance_season, :song_name, presence: true
+  validates :dancer_id, :dance_season, :song_name, presence: true
 
   # Gets current costume assignments for a dancer
   scope :current_dancer_costumes, lambda { |dancer| where(["dancer_id = '%s' and dance_season = '%s'", dancer.id, Time.now.year]) }
