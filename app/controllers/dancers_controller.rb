ab6e2dfffba407a48dcc64fa543bea84662e5c04
jamieberrier <berrier.jamie@gmail.com>
2020-04-29 13:10:28 -0700

    add comments before actions

--- app/controllers/dancers_controller.rb
+++ app/controllers/dancers_controller.rb
@@ -1,13 +1,15 @@
 class DancersController < ApplicationController
   skip_before_action :require_logged_in!, only: :create
   before_action :redirect_if_not_studio_owner!, only: %i[new index current_dancers]
-  before_action :redirect_if_not_studio_dancer!, only: %i[show edit destroy current_assignments]
+  before_action :redirect_if_not_studio_dancer!, only: %i[show edit update destroy current_assignments]
 
-  # For Dance Studio to create a new dancer
+  # Displays from for a Dance Studio to create a new dancer
   def new
     @dancer = Dancer.new(dance_studio_id: current_user.id, password: 'password', password_confirmation: 'password')
   end
 
+  # Receives data from registrations/new (renders dancers/_form)
+  # dance studio can also create dancer account: dancers/new (renders _form)
   def create
     @dancer = Dancer.new(dancer_params)
 
@@ -22,16 +24,20 @@ class DancersController < ApplicationController
     end
   end
 
-  # Uses instance variable b/c dance_studio can also view
+  # Display dancer's show page
+  # dancer & dance studio can view
   def show
     find_dancer
   end
 
-  # Uses instance variable b/c dance_studio can also edit
+  # Displays form to edit dancer's account info
+  # dancer & dance studio can edit
   def edit
     find_dancer
   end
 
+  # Receives data from edit form
+  # dancer & dance studio can update
   def update
     find_dancer
     if @dancer.update(dancer_params)
@@ -41,11 +47,14 @@ class DancersController < ApplicationController
     end
   end
 
-  # Only dance_studio can view
+  # Displays all dancers for a dance studio
+  # only dance_studio can view
   def index
     # all dancers at a dance_studio
   end
 
+  # Deactivates dancer's account to keep costume assignment data
+  # dancer & dance studio can deactivate
   def destroy
     find_dancer
     # set current_dancer to false & password/confirmation to "..."
@@ -63,12 +72,14 @@ class DancersController < ApplicationController
   end
 
   # Displays dancer's current costume assignments with costume picture
+  # dancer & dance studio can view
   def current_assignments
     find_dancer
     @assignments = CostumeAssignment.current_dancer_costumes(@dancer)
   end
 
   # Displays the current dancers for a dance studio
+  # only dance studio can view
   def current_dancers
     @dancers = Dancer.current_dancers(current_user)
   end
