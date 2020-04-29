a256088aecf2b7e3dad721b326eafe776090aae6
jamieberrier <berrier.jamie@gmail.com>
2020-04-29 13:11:48 -0700

    update before action helpers

--- app/controllers/costume_assignments_controller.rb
+++ app/controllers/costume_assignments_controller.rb
@@ -1,22 +1,30 @@
 class CostumeAssignmentsController < ApplicationController
-  before_action :require_logged_in!
-  before_action :owner?, only: %i[index costume_assignments]
+  before_action :redirect_if_not_owner_assigned!, only: :costume_assignments
+  before_action :redirect_if_not_studio_owner!, only: :index
+  before_action :redirect_if_not_studio_dancer!, only: :dancer_assignments
 
+  # nested --> params[:dance_studio_id]
+  # url: dance_studios/1/costume_assignments
   # owner viewing all of its dance studio's costume assignments
   def index
     @costume_assignments = current_user.costume_assignments.order(dance_season: :desc, genre: :asc, song_name: :asc)
   end
 
   # TODO: should this be in costumes controller?
-  # owner viewing a costume's assignments
-  # params[:costume_id]
+  # unnested
+  # costume id --> params[:id]
+  # url: /costumes/3/assignments
+  # owner viewing all of a costume's assignments
   def costume_assignments
     find_costume
     @costume_assignments = CostumeAssignment.where(costume_id: @costume)
   end
 
   # TODO: should this be in dancers controller?
-  # dancer viewing all of his/her costume assignments / owner viewing a dancer's costume assignments -- params[:id]
+  # unnested
+  # dancer id --> params[:id]
+  # url: /dancers/3/costume_assignments
+  # dancer viewing all costume assignments / owner viewing a dancer's costume assignments
   def dancer_assignments
     find_dancer
     @costume_assignments = CostumeAssignment.where(dancer_id: @dancer).order(dance_season: :desc, genre: :asc, song_name: :asc)
