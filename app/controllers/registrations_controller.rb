58f6f51a62e91b25129389fec5e6048ba8be3abd
jamieberrier <berrier.jamie@gmail.com>
2020-04-29 13:09:52 -0700

    update skip before action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,5 @@
 class RegistrationsController < ApplicationController
-  skip_before_action :require_logged_in!, only: %i[new google_auth]
+  skip_before_action :require_logged_in!
 
   def new
     redirect_if_logged_in!
