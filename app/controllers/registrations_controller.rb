27a56360c499a1c1768ab4aea91472acc43c30dc
jamieberrier <berrier.jamie@gmail.com>
2020-05-01 12:50:58 -0700

    delete google_auth action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,15 +2,7 @@ class RegistrationsController < ApplicationController
   skip_before_action :require_logged_in
 
   def new
-    redirect_if_logged_in
+    redirect_if_logged_in('You are already logged in')
     create_empty_user
-  end
-
-  def google_auth
-    if session[:dance_studio]
-      @dance_studio = DanceStudio.new(session[:dance_studio])
-    else
-      @dancer = Dancer.new(session[:dancer])
-    end
   end
 end
