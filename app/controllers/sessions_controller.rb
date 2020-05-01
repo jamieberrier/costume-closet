f3109e39429602bce7728de0237a9a71a40e15b6
jamieberrier <berrier.jamie@gmail.com>
2020-05-01 12:57:12 -0700

    dry up gooth_auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,30 +24,17 @@ class SessionsController < ApplicationController
     # Access_token is used to authenticate request made from the rails application to the google server
     access_token = request.env['omniauth.auth']
     user_type = request.env['omniauth.params']['user_type']
-    session[:dance_studio] = nil
-    session[:dancer] = nil
 
-    if user_type == 'dance_studio'
-      @dance_studio = DanceStudio.from_omniauth(access_token)
-    else
-      @dancer = Dancer.from_omniauth(access_token)
-    end
-    if @dance_studio
-      if @dance_studio.id
-        @dance_studio.save
-        log_in(@dance_studio, 'Successfully logged in!')
-      else # if signing up
-        session[:dance_studio] = @dance_studio
-        redirect_to dance_studio_google_register_path
-      end
-    else
-      if @dancer.id
-        @dancer.save
-        log_in(@dancer, 'Successfully logged in!')
-      else # if signing up
-        session[:dancer] = @dancer
-        redirect_to dancer_google_register_path
-      end
+    @dance_studio = DanceStudio.from_omniauth(access_token) if user_type == 'dance_studio'
+    @dancer = Dancer.from_omniauth(access_token) if user_type == 'dancer'
+
+    # try is an ActiveSupport method: object.try(:some_method) means if object != nil then object.some_method else nil end.
+    if @dance_studio.try(:id)
+      log_in(@dance_studio, 'Successfully logged in!')
+    elsif @dancer.try(:id)
+      log_in(@dancer, 'Successfully logged in!')
+    else # if signing up
+      render 'registrations/google_auth'
     end
   end
 
