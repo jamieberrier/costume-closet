834434652a06b806c8873f7f12219d9f0900e16c
jamieberrier <berrier.jamie@gmail.com>
2020-05-04 15:20:27 -0700

    add costume size and condition to validation

--- app/models/costume_assignment.rb
+++ app/models/costume_assignment.rb
@@ -1,8 +1,8 @@
 class CostumeAssignment < ApplicationRecord
   belongs_to :dancer
   belongs_to :costume
-  # costume_size, genre, hair_accessory, shoe, tight, costume_condition
-  validates :dancer_id, :dance_season, :song_name, presence: true
+  # genre, hair_accessory, shoe, tight
+  validates :dancer_id, :costume_size, :costume_condition, :dance_season, :song_name, presence: true
 
   # Gets current costume assignments for a dancer
   scope :current_dancer_costumes, lambda { |dancer| where(["dancer_id = '%s' and dance_season = '%s'", dancer.id, Time.now.year]) }
