22ab9e46ce8dade4f55d58112b24171057873552
jamieberrier <berrier.jamie@gmail.com>
2020-05-05 13:56:20 -0700

    refactor code

--- app/controllers/dancers_controller.rb
+++ app/controllers/dancers_controller.rb
@@ -10,56 +10,56 @@ class DancersController < ApplicationController
     @dancer = Dancer.new(dance_studio_id: current_user.id, password: 'password', password_confirmation: 'password')
   end
 
-  # Receives data from registrations/new (renders dancers/_form)
-  # dance studio can also create dancer account: dancers/new (renders _form)
+  # Dancer & Owner
+  # Dancer: receives data from registrations/new (renders dancers/_form)
+  # Dance Studio: receives data from dancers/new (renders _form)
   def create
     @dancer = Dancer.new(dancer_params)
+    saved = @dancer.save
 
-    if @dancer.save && owner?
-      redirect_to_dance_studio_page('Dancer Added!')
-    elsif @dancer.save
-      log_in(@dancer, 'Successfully Registered!')
-    elsif owner?
-      render_new_dancer_form
-    else
-      render_registration_form(@dancer)
-    end
+    return redirect_to_dance_studio_page('Dancer Added!') if saved && owner?
+
+    return log_in(@dancer, 'Successfully Registered!') if saved
+
+    return render_new_dancer_form if owner?
+
+    render_registration_form(@dancer)
   end
 
+  # Dancer & Owner
   # Display dancer's show page
-  # dancer & dance studio can view
   # url: /dancers/1
   def show
     find_dancer
   end
 
+  # Dancer
   # Displays form to edit dancer's account info
-  # dancer & dance studio can edit
   # url: /dancers/1/edit
   def edit
     find_dancer
   end
 
+  # Dancer
   # Receives data from edit form
-  # dancer & dance studio can update
   def update
     find_dancer
-    if @dancer.update(dancer_params)
-      redirect_to dancer_path(@dancer), success: 'Account Info Updated!'
-    else
-      render_edit_dancer_form
-    end
+
+    return render_edit_dancer_form unless @dancer.update(dancer_params)
+
+    redirect_to dancer_path(@dancer), success: 'Account Info Updated!'
   end
 
-  # dancer viewing all costume assignments / owner viewing a dancer's costume assignments
+  # Dancer & Owner
+  # Displays all of dancer's costume assignments
   # url: /dancers/3/costume_assignments
   def dancer_assignments
     find_dancer
-    @costume_assignments = CostumeAssignment.where(dancer_id: @dancer).order(dance_season: :desc, genre: :asc, song_name: :asc)
+    @assignments = CostumeAssignment.where(dancer_id: @dancer).order(dance_season: :desc, genre: :asc, song_name: :asc)
   end
 
+  # Dancer & Owner
   # Displays dancer's current costume assignments with costume picture
-  # dancer & dance studio can view
   # url: /dancers/1/current_assignments
   def current_assignments
     find_dancer
@@ -68,32 +68,28 @@ class DancersController < ApplicationController
 
   # Owner
   # Displays all dancers for a dance studio
-  # only dance_studio can view
   # url: /dance_studios/1/dancers
   def index
-    # all dancers at a dance_studio
+    @dancers = current_user.dancers.order(:last_name, :first_name)
   end
 
   # Owner
   # Displays the current dancers for a dance studio
-  # only dance studio can view
   # url: /dance_studios/1/dancers/current_dancers
   def current_dancers
-    @dancers = Dancer.current_dancers(current_user)
+    @dancers = Dancer.current_dancers(current_user).order(:last_name, :first_name)
   end
 
+  # Dancer & Owner
   # Deactivates dancer's account to keep costume assignment data
-  # dancer & dance studio can deactivate
   def destroy
     find_dancer
     # set current_dancer to false & password/confirmation to 'dancer'
     @dancer.update(current_dancer: false, password: 'dancer', password_confirmation: 'dancer')
 
-    if dancer?
-      logout('Account Deactivated!')
-    else # if studio owner deactivating a dancer account
-      redirect_to_dance_studio_page('Account Deactivated')
-    end
+    return redirect_to_dance_studio_page('Account Deactivated') unless dancer?
+
+    logout('Account Deactivated!')
   end
 
   private
