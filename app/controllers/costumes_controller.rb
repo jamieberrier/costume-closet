5268427a3be9a5943097f3b2453c250f19b52997
jamieberrier <berrier.jamie@gmail.com>
2020-05-06 11:01:39 -0700

    use helpers as before_actions to clean up actions and refactor assign action

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -2,12 +2,12 @@ class CostumesController < ApplicationController
   before_action :require_dance_studio_owner, only: %i[new create index]
   before_action :require_costume_ownership, only: %i[show]
   before_action :require_studio_costume, except: %i[new create index show]
+  before_action :find_costume, except: %i[new create index delete_season_assignments]
+  before_action :find_season_costume_assignments, only: %i[season_assignments edit_season_assignments]
 
   # Dance Studio & Dancer can view
   # url: /costumes/3
-  def show
-    find_costume
-  end
+  def show; end
 
   # url: /dance_studios/1/costumes/new
   def new
@@ -38,19 +38,15 @@ class CostumesController < ApplicationController
   end
 
   # url: /costumes/3/edit
-  def edit
-    find_costume
-  end
+  def edit; end
 
   def update
-    find_costume
     return redirect_to edit_costume_path(@costume, dance_studio: current_user), danger: "Edit failure: #{@costume.errors.full_messages.to_sentence}" unless update_costume
 
     redirect_to_costume_path('Costume Updated!')
   end
 
   def destroy
-    find_costume
     @costume.destroy
 
     redirect_to dance_studio_costumes_path(current_user), success: 'Costume Deleted'
@@ -59,57 +55,50 @@ class CostumesController < ApplicationController
   # Displays form to assign a costume
   # url: /costumes/5/assign
   def assign_costume
-    find_costume
     build_shared_assignment_info
     build_assignments_with_dancer_id
   end
 
   # Receives data from costume assignment form
   def assign
-    find_costume
-    updated = update_costume
+    # gets the paramater for key '1'
+    assignment_info = params[:costume][:costume_assignments_attributes].fetch('1')
+    # => <ActionController::Parameters {"dance_season"=>"2020", "song_name"=>"test", "genre"=>"", "hair_accessory"=>"", "shoe"=>"", "tight"=>""} permitted: false>
 
-    if updated && @costume.costume_assignments.empty?
-      redirect_to assign_costume_path(@costume), danger: 'Assignment failure: Must fill out Dance Season & Song Name AND select at least 1 dancer w/ costume size & costume condition'
-    elsif updated
-      redirect_to season_assignments_path(@costume, season: @costume.costume_assignments.last.dance_season)
-    else
-      redirect_to assign_costume_path(@costume), danger: "Assignment failure: #{@costume.errors.full_messages.to_sentence}"
-    end
+    # checks if the dance_season or song_name value is empty
+    return redirect_to assign_costume_path(@costume), danger: 'Assignment failure: Must fill out Dance Season & Song Name AND select at least 1 dancer w/ costume size & costume condition' if assignment_info[:dance_season].empty? || assignment_info[:song_name].empty?
+
+    count = @costume.costume_assignments.count # gets no. of assignments before updating
+    updated = update_costume # try to persist to db
+
+    # check that if updates, @costume.costume_assignments.count is now greater than count
+    return redirect_to assign_costume_path(@costume), danger: 'Assignment failure: Must fill out Dance Season & Song Name AND select at least 1 dancer w/ costume size & costume condition' if updated && count == @costume.costume_assignments.count
+    return redirect_to season_assignments_path(@costume, season: @costume.costume_assignments.last.dance_season) if updated
+
+    redirect_to assign_costume_path(@costume), danger: "Assignment failure: #{@costume.errors.full_messages.to_sentence}"
   end
 
   # owner viewing all of a costume's assignments
   # url: /costumes/3/assignments
   def costume_assignments
-    find_costume
     @assignments = CostumeAssignment.where(costume_id: @costume)
   end
 
   # owner viewing a costume's assignments for a season
   # params[:season]
   # url: /costumes/1/season_assignments?season=2020
-  def season_assignments
-    find_costume
-    @season = params[:season]
-    find_season_costume_assignments
-  end
+  def season_assignments; end
 
   # Displays form for owner to edit a costume's assignments for a season
   # params[:season]
   # url: /costumes/1/edit_season_assignments?season=2020
   def edit_season_assignments
-    find_costume
-    @season = params[:season]
-    find_season_costume_assignments
     @assignment_info = @costume.costume_assignments.build(hair_accessory: @assignments.first.hair_accessory, tight: @assignments.first.tight, shoe: @assignments.first.shoe, genre: @assignments.first.genre, song_name: @assignments.first.song_name, costume_id: @costume.id, dance_season: @season, id: nil, dancer_id: nil, costume_size: nil, costume_condition: nil)
-    # build a costume_assignments record unless one with dancer's id already exists
-    Dancer.current_dancers(current_user).each do |dancer|
-      @costume.costume_assignments.build(dancer_id: dancer.id) unless @assignments.exists?(dancer_id: dancer.id)
-    end
+
+    build_assignments_unless_exists
   end
 
   def update_season_assignments
-    find_costume
     update_costume
 
     redirect_to season_assignments_path(@costume, season: params[:costume][:costume_assignments_attributes].values[0].values[0])
@@ -119,6 +108,7 @@ class CostumesController < ApplicationController
   # params[:season], params[:id] -> costume id
   def delete_season_assignments
     CostumeAssignment.where("costume_id = '%s' and dance_season = '%s'", params[:id], params[:season]).destroy_all
+
     redirect_to costume_path(params[:id]), success: 'Assignments Deleted'
   end
 
