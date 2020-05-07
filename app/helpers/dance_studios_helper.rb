74e3ea14de1e907139a46e2186f23a6e2f39210a
jamieberrier <berrier.jamie@gmail.com>
2020-05-07 13:42:38 -0700

    rearrange actions

--- app/helpers/dance_studios_helper.rb
+++ app/helpers/dance_studios_helper.rb
@@ -1,4 +1,11 @@
 module DanceStudiosHelper
+  # Studio
+  # unnested, params[:id] -> dance studio id
+  # dance_studios -- show edit update destroy current_assignments current_costumes unassigned_costumes
+  def require_studio_ownership
+    redirect_to root_path(message: 'Only dance studio owner can access') unless owner? && current_user.id == params[:id].to_i
+  end
+
   def find_dance_studio
     @dance_studio = DanceStudio.find(params[:id])
   end
@@ -11,12 +18,5 @@ module DanceStudiosHelper
     flash.now[:danger] = "Edit failure: #{@dance_studio.errors.full_messages.to_sentence}"
 
     render 'dance_studios/edit'
-  end
-
-  # Owner
-  # unnested, params[:id] -> dance studio id
-  # dance_studios -- show edit update destroy current_assignments current_costumes unassigned_costumes
-  def require_studio_ownership
-    redirect_to root_path(message: 'Only dance studio owner can access') unless owner? && current_user.id == params[:id].to_i
   end
 end
