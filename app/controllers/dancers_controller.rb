4d865856c816bb3ba8b755bfe93b8d3d57469214
jamieberrier <berrier.jamie@gmail.com>
2020-05-07 13:52:33 -0700

    refactor create action and rearrange actions

--- app/controllers/dancers_controller.rb
+++ app/controllers/dancers_controller.rb
@@ -4,38 +4,45 @@ class DancersController < ApplicationController
   before_action :require_studio_dancer, except: %i[new create index current_dancers]
   before_action :find_dancer, except: %i[new create index current_dancers]
 
-  # Owner
+  # Studio
+  # Displays all dancers for a dance studio
+  # url: /dance_studios/1/dancers
+  def index
+    @dancers = current_user.dancers.order(:last_name, :first_name)
+  end
+
+  # Dancer & Studio
+  # Display dancer's show page
+  # url: /dancers/1
+  def show; end
+
+  # Studio
   # Displays from for a Dance Studio to create a new dancer
   # url: /dance_studios/1/dancers/new
   def new
     @dancer = Dancer.new(dance_studio_id: current_user.id, password: 'password', password_confirmation: 'password')
   end
 
-  # Dancer & Owner
-  # Dancer: receives data from registrations/new (renders dancers/_form)
-  # Dance Studio: receives data from dancers/new (renders _form)
-  def create
-    @dancer = Dancer.new(dancer_params)
-    saved = @dancer.save
-
-    return redirect_to_dance_studio_page('Dancer Added!') if saved && owner?
-
-    return log_in(@dancer, 'Successfully Registered!') if saved
-
-    return render_new_dancer_form if owner?
-
-    render_registration_form(@dancer)
-  end
-
-  # Dancer & Owner
-  # Display dancer's show page
-  # url: /dancers/1
-  def show; end
-
   # Dancer
   # Displays form to edit dancer's account info
   # url: /dancers/1/edit
   def edit; end
+
+  # Dancer & Studio
+  # Dancer: receives data from registrations/new (renders dancers/_form)
+  # Dance Studio: receives data from dancers/new (renders _form)
+  def create
+    @dancer = Dancer.new(dancer_params)
+    @saved = @dancer.save
+
+    redirect_studio_if_saved && return
+
+    log_in_dancer_if_saved && return
+
+    redirect_studio_if_error && return
+
+    render_registration_form(@dancer)
+  end
 
   # Dancer
   # Receives data from edit form
@@ -45,35 +52,7 @@ class DancersController < ApplicationController
     redirect_to dancer_path(@dancer), success: 'Account Info Updated!'
   end
 
-  # Dancer & Owner
-  # Displays all of dancer's costume assignments
-  # url: /dancers/3/costume_assignments
-  def dancer_assignments
-    @assignments = CostumeAssignment.where(dancer_id: @dancer).order(dance_season: :desc, genre: :asc, song_name: :asc)
-  end
-
-  # Dancer & Owner
-  # Displays dancer's current costume assignments with costume picture
-  # url: /dancers/1/current_assignments
-  def current_assignments
-    @assignments = CostumeAssignment.current_dancer_costumes(@dancer)
-  end
-
-  # Owner
-  # Displays all dancers for a dance studio
-  # url: /dance_studios/1/dancers
-  def index
-    @dancers = current_user.dancers.order(:last_name, :first_name)
-  end
-
-  # Owner
-  # Displays the current dancers for a dance studio
-  # url: /dance_studios/1/dancers/current_dancers
-  def current_dancers
-    @dancers = Dancer.current_dancers(current_user).order(:last_name, :first_name)
-  end
-
-  # Dancer & Owner
+  # Dancer & Studio
   # Deactivates dancer's account to keep costume assignment data
   def destroy
     # set current_dancer to false & password/confirmation to 'dancer'
@@ -82,6 +61,27 @@ class DancersController < ApplicationController
     return redirect_to_dance_studio_page('Account Deactivated') unless dancer?
 
     logout('Account Deactivated!')
+  end
+
+  # Dancer & Studio
+  # Displays all of dancer's costume assignments
+  # url: /dancers/3/costume_assignments
+  def dancer_assignments
+    @assignments = CostumeAssignment.where(dancer_id: @dancer).order(dance_season: :desc, genre: :asc, song_name: :asc)
+  end
+
+  # Dancer & Studio
+  # Displays dancer's current costume assignments with costume picture
+  # url: /dancers/1/current_assignments
+  def current_assignments
+    @assignments = CostumeAssignment.current_dancer_costumes(@dancer)
+  end
+
+  # Studio
+  # Displays the current dancers for a dance studio
+  # url: /dance_studios/1/dancers/current_dancers
+  def current_dancers
+    @dancers = Dancer.current_dancers(current_user).order(:last_name, :first_name)
   end
 
   private
