d16c9daff1f1cd8759857c15c72211a19da1a183
jamieberrier <berrier.jamie@gmail.com>
2020-05-07 13:46:23 -0700

    add custom method to validate email is not taken by a dancer

--- app/models/dance_studio.rb
+++ app/models/dance_studio.rb
@@ -1,6 +1,6 @@
 class DanceStudio < ApplicationRecord
   # adds methods, ie: dancers, dancers<<, dancers.delete, dancers_ids=ids
-  has_many :dancers, dependent: :destroy # causes all the associated objects to also be destroyed
+  has_many :dancers, dependent: :destroy # all associated objects also destroyed
   # adds methods, ie: costumes, costumes<<, costumes.delete, costumes_ids=ids
   has_many :costumes, dependent: :destroy
   # adds methods, ie: costume_assignment_ids=ids
@@ -9,10 +9,20 @@ class DanceStudio < ApplicationRecord
 
   has_secure_password
 
+  validate :email_not_taken, on: :create
+  validate :email_not_taken, on: :update
+
   validates :password, confirmation: { case_sensitive: false }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :studio_name, :owner_name, :password_confirmation, presence: true
 
+  def email_not_taken
+    Dancer.all.each do |dancer|
+      errors.add(:email, 'is taken') if dancer.email.downcase == email.downcase
+    end
+  end
+
+  # Omniauth - google
   def self.from_omniauth(auth)
     # Creates a new user only if it doesn't exist
     where(email: auth.info.email).first_or_initialize do |user|
@@ -28,7 +38,7 @@ class DanceStudio < ApplicationRecord
 
   # Gets the current costumes for a dance studio
   def current_studio_costumes
-    Costume.find_by_assignment(costume_assignments.where("dance_season = '%s'", Time.now.year))
+    Costume.find_by_assignment(current_studio_assignments)
   end
 
   # Gets the currently unassigned costumes for a dance studio
