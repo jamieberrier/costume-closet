92a48612c52293cabfacedf2a31f0ab9344ebe3d
jamieberrier <berrier.jamie@gmail.com>
2020-05-07 13:49:43 -0700

    move unassigned? helper to costumes helper from costume assignments helper

--- app/helpers/costume_assignments_helper.rb
+++ app/helpers/costume_assignments_helper.rb
@@ -1,13 +1,4 @@
 module CostumeAssignmentsHelper
-  def find_assignment
-    @costume_assignment = CostumeAssignment.find(params[:id])
-  end
-
-  def unassigned?
-    # return true if @costume has no rows w/ 2020 dance season in CostumeAssignments table
-    current_user.unassigned_studio_costumes.include?(@costume)
-  end
-
   # Owner
   # nested, params[:dance_studio_id] -> dance studio id
   # dancers -- new index current dancers
--- app/helpers/costumes_helper.rb
+++ app/helpers/costumes_helper.rb
@@ -1,10 +1,30 @@
 module CostumesHelper
+  # Studio & Dancer: params[:id] -> costume id
+  # costumes -- show
+  def require_costume_ownership
+    # check if costume belongs to studio or assigned to dancer
+    redirect_to root_path(message: 'Only can owner or assigned dancer can access') unless current_user.costumes.include?(find_costume)
+  end
+
+  # Studio
+  # no dance studio id, params[:id] -> costume id
+  # costumes -- edit update destroy assign_costume assign costume_assignments season_assignments edit_eason_assignments update_season_assignments delete_season_assignments
+  def require_studio_costume
+    redirect_to root_path(message: 'Only costume owner can access') unless owner? && current_user.costumes.include?(find_costume)
+  end
+
   def find_costume
     @costume = Costume.find(params[:id])
   end
 
-  def redirect_to_costume_path(message)
-    redirect_to costume_path(@costume), success: message
+  def find_season_costume_assignments
+    @season = params[:season]
+    @assignments = CostumeAssignment.where("costume_id = '%s' and dance_season = '%s'", @costume.id, @season)
+  end
+
+  # instanstiates an empty instance of costume assignment - to collect the shared data for the assignments
+  def build_shared_assignment_info
+    @assignment_info = @costume.costume_assignments.build
   end
 
   # instanstiates an instance of costume assignment for each current dancer w/ the dancer's id
@@ -14,6 +34,16 @@ module CostumesHelper
     end
   end
 
+  def redirect_to_costume_path(message)
+    redirect_to costume_path(@costume), success: message
+  end
+
+  def render_edit_costume_form
+    flash.now[:danger] = "Edit failure: #{@costume.errors.full_messages.to_sentence}"
+
+    render 'costumes/edit'
+  end
+
   # build a costume_assignments record unless one with dancer's id already exists
   def build_assignments_unless_exists
     Dancer.current_dancers(current_user).each do |dancer|
@@ -21,27 +51,22 @@ module CostumesHelper
     end
   end
 
-  def find_season_costume_assignments
-    @season = params[:season]
-    @assignments = CostumeAssignment.where("costume_id = '%s' and dance_season = '%s'", @costume.id, @season)
-  end
-
   def update_costume
     @costume.update(costume_params)
   end
 
-  # instanstiates an empty instance of costume assignment - to collect the shared data for the assignments
-  def build_shared_assignment_info
-    @assignment_info = @costume.costume_assignments.build
+  # return true if @costume has no rows w/ 2020 dance season in CostumeAssignments table
+  def unassigned?
+    current_user.unassigned_studio_costumes.include?(@costume)
   end
 
+  ## create action helpers
   def redirect_to_new_costume_form(message)
     redirect_to new_dance_studio_costume_path(current_user.id), message
   end
 
-  ## create action helpers
   def redirect_if_validation_error
-    redirect_to new_dance_studio_costume_path(current_user.id), danger: "Creation failure: #{@costume.errors.full_messages.to_sentence}" unless @costume.save
+    redirect_to_new_costume_form(danger: "Creation failure: #{@costume.errors.full_messages.to_sentence}") unless @costume.save
   end
 
   def redirect_if_not_assigning
@@ -61,6 +86,7 @@ module CostumesHelper
     @song_name_empty = @assignment_info[:song_name].empty?
     @count = @costume.costume_assignments.count # gets no. of assignments before updating
   end
+
   # checks if the dance_season or song_name value is empty
   def redirect_if_required_values_empty
     if params[:action] == 'create'
@@ -79,19 +105,5 @@ module CostumesHelper
   # redirect if updated correctly
   def redirect_if_updated
     redirect_to season_assignments_path(@costume, season: @costume.costume_assignments.last.dance_season) if @updated
-  end
-
-  # Owner
-  # no dance studio id, params[:id] -> costume id
-  # costumes -- edit update destroy assign_costume assign costume_assignments season_assignments edit_eason_assignments update_season_assignments delete_season_assignments
-  def require_studio_costume
-    redirect_to root_path(message: 'Only costume owner can access') unless owner? && current_user.costumes.include?(find_costume)
-  end
-
-  # Owner & Dancer: params[:id] -> costume id
-  # costumes -- show
-  def require_costume_ownership
-    # check if costume belongs to studio or assigned to dancer
-    redirect_to root_path(message: 'Only can owner or assigned dancer can access') unless current_user.costumes.include?(find_costume)
   end
 end
