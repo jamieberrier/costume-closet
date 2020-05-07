90449ddf320086ddc6f890618e212f5a8392462d
jamieberrier <berrier.jamie@gmail.com>
2020-05-07 13:43:48 -0700

    rearrange actions

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,18 +1,18 @@
 module RegistrationsHelper
-  def create_empty_user
-    if signing_up_as_dance_studio?
-      @dance_studio = DanceStudio.new
-    else
-      @dancer = Dancer.new
-    end
-  end
-
   def signing_up_as_dance_studio?
     params[:user_type] == 'dance_studio'
   end
 
   def signing_up_as_dancer?
     params[:user_type] == 'dancer'
+  end
+
+  def create_empty_user
+    if signing_up_as_dance_studio?
+      @dance_studio = DanceStudio.new
+    else
+      @dancer = Dancer.new
+    end
   end
 
   def render_registration_form(new_user)
