23116ef7c78e95d79b2459a2b305f282d38d575a
jamieberrier <berrier.jamie@gmail.com>
2020-05-07 13:44:24 -0700

    add comments

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,7 +1,7 @@
 class Costume < ApplicationRecord
   belongs_to :dance_studio
   # adds methods, ie: costume_assignments, costume_assignments<<, costume_assignments.delete, costume_assignments_ids=ids
-  has_many :costume_assignments, dependent: :delete_all # the join records will be removed when a costume is destroyed
+  has_many :costume_assignments, dependent: :delete_all # join records removed when costume destroyed
   # adds methods, ie: dancer_ids=ids
   has_many :dancers, through: :costume_assignments
 
@@ -13,71 +13,36 @@ class Costume < ApplicationRecord
   # Gets assigned costumes to display in dance_studios/:id/current_assignments
   scope :find_by_assignment, lambda { |assignments| assignments.group(:costume_id).collect { |x| find(x.costume_id) } }
 
+  # Returns true if onepiece_description is blank
   def onepiece_blank?
     onepiece_description.blank?
   end
 
+  # Returns true if top_description & bottoms_description are blank
   def twopiece_blank?
     top_description.blank? && bottoms_description.blank?
   end
 
+  # Returns true if onepiece_description is filled in
   def onepiece_costume?
     onepiece_description.present?
   end
 
+  # Returns true if top_description & bottoms_description are filled in
   def twopiece_costume?
     top_description.present? && bottoms_description.present?
   end
 
+  # Concatenates the top description w/ the bottoms description for displaying
   def twopiece_description
     top_description + ' with ' + bottoms_description
   end
 
+  # Gets the costume's assigned seasons w/ no. of assignments per season
   def seasons
     costume_assignments.group(:dance_season).count
   end
 
-=begin
-assign
-  assignments_hashes
-=> {"1"=>{"song_name"=>"test", "dance_season"=>"2020", "genre"=>"", "hair_accessory"=>"", "shoe"=>"", "tight"=>""},
- "10"=>{"dancer_id"=>"0", "costume_size"=>"", "costume_condition"=>"New"},
- "11"=>{"dancer_id"=>"0", "costume_size"=>"", "costume_condition"=>"New"},
- "12"=>{"dancer_id"=>"0", "costume_size"=>"", "costume_condition"=>"New"},
- "13"=>{"dancer_id"=>"0", "costume_size"=>"", "costume_condition"=>"New"},
- "14"=>{"dancer_id"=>"0", "costume_size"=>"", "costume_condition"=>"New"},
- "15"=>{"dancer_id"=>"0", "costume_size"=>"", "costume_condition"=>"New"}}
-
-edit season assignments
-  assignments_hashes
-=> {"5"=>
-  {"song_name"=>"test",
-   "dance_season"=>"2020",
-   "genre"=>"lyrical",
-   "hair_accessory"=>"none",
-   "shoe"=>"none",
-   "tight"=>"none"},
- "6"=>{"id"=>"25", "dancer_id"=>"0", "costume_size"=>"M"},
- "7"=>{"id"=>"26", "dancer_id"=>"2", "costume_size"=>"XS"},
- "8"=>{"id"=>"27", "dancer_id"=>"3", "costume_size"=>"S"},
- "9"=>{"id"=>"28", "dancer_id"=>"4", "costume_size"=>"M"},
- "10"=>{"id"=>"29", "dancer_id"=>"5", "costume_size"=>"M"}}
-
- new
-    assignments_hashes
-=> {"0"=>
-  {"song_name"=>"test",
-   "dance_season"=>"2020",
-   "genre"=>"lyrical",
-   "hair_accessory"=>"none",
-   "shoe"=>"none",
-   "tight"=>"none"},
- "7"=>{"dancer_id"=>"0", "costume_size"=>""},
- "8"=>{"dancer_id"=>"0", "costume_size"=>""},
- "9"=>{"dancer_id"=>"3", "costume_size"=>"S"},
- "10"=>{"dancer_id"=>"0", "costume_size"=>""},
- "11"=>{"dancer_id"=>"0", "costume_size"=>""}}
-=end
   def costume_assignments_attributes=(assignments_hashes)
     # get shared data and remove from hash (song_name, genre, etc.)
     shared_attributes = assignments_hashes.shift.pop
@@ -101,3 +66,54 @@ edit season assignments
     end
   end
 end
+
+=begin
+
+ new
+    assignments_hashes
+=> {"0"=>
+  {"song_name"=>"Golden",
+    "dance_season"=>"2020",
+    "genre"=>"lyrical",
+    "hair_accessory"=>"none",
+    "shoe"=>"none",
+    "tight"=>"none"},
+ "8"=>{"dancer_id"=>"0", "costume_size"=>""},
+ "9"=>{"dancer_id"=>"0", "costume_size"=>""},
+ "10"=>{"dancer_id"=>"0", "costume_size"=>""},
+ "11"=>{"dancer_id"=>"4", "costume_size"=>"M"},
+ "12"=>{"dancer_id"=>"5", "costume_size"=>"M"},
+ "13"=>{"dancer_id"=>"0", "costume_size"=>""}}
+
+assign
+  assignments_hashes
+=> {"1"=>
+  {"song_name"=>"Swan Lake",
+   "dance_season"=>"2020",
+   "genre"=>"ballet",
+   "hair_accessory"=>"tiara",
+   "shoe"=>"pink ballet",
+   "tight"=>"pink footed"},
+ "10"=>{"dancer_id"=>"1", "costume_size"=>"S", "costume_condition"=>"Used"},
+ "11"=>{"dancer_id"=>"2", "costume_size"=>"S", "costume_condition"=>"New"},
+ "12"=>{"dancer_id"=>"0", "costume_size"=>"", "costume_condition"=>""},
+ "13"=>{"dancer_id"=>"0", "costume_size"=>"", "costume_condition"=>""},
+ "14"=>{"dancer_id"=>"0", "costume_size"=>"", "costume_condition"=>""},
+ "15"=>{"dancer_id"=>"15", "costume_size"=>"M", "costume_condition"=>"New"}}
+
+edit season assignments
+  assignments_hashes
+=> {"4"=>
+  {"song_name"=>"Swan Lake",
+   "dance_season"=>"2020",
+   "genre"=>"ballet",
+   "hair_accessory"=>"tiara",
+   "shoe"=>"pink ballet",
+   "tight"=>"pink footed"},
+ "9"=>{"id"=>"27", "dancer_id"=>"1", "costume_size"=>"S", "costume_condition"=>"Used"},
+ "10"=>{"id"=>"28", "dancer_id"=>"2", "costume_size"=>"S", "costume_condition"=>"New"},
+ "11"=>{"id"=>"29", "dancer_id"=>"0", "costume_size"=>"M", "costume_condition"=>"New"},
+ "21"=>{"dancer_id"=>"3", "costume_size"=>"M", "costume_condition"=>"New"},
+ "22"=>{"dancer_id"=>"0", "costume_size"=>"", "costume_condition"=>""},
+ "23"=>{"dancer_id"=>"0", "costume_size"=>"", "costume_condition"=>""}}
+=end
