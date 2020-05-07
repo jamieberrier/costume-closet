3cf22bff2e35599bf36aa9eeefb5651827ebdadc
jamieberrier <berrier.jamie@gmail.com>
2020-05-07 13:53:11 -0700

    make find_dance_studio a before_action

--- app/controllers/dance_studios_controller.rb
+++ app/controllers/dance_studios_controller.rb
@@ -1,6 +1,13 @@
 class DanceStudiosController < ApplicationController
   skip_before_action :require_logged_in, only: :create
   before_action :require_studio_ownership, except: :create
+  before_action :find_dance_studio, only: %i[show edit update destroy]
+
+  # url: /dance_studios/1
+  def show; end
+
+  # url: /dance_studios/1/edit
+  def edit; end
 
   def create
     @dance_studio = DanceStudio.new(dance_studio_params)
@@ -10,31 +17,18 @@ class DanceStudiosController < ApplicationController
     log_in(@dance_studio, 'Successfully Registered!')
   end
 
-  # url: /dance_studios/1
-  def show
-
-  end
-
-  # url: /dance_studios/1/edit
-  def edit
-    find_dance_studio
-  end
-
   def update
-    find_dance_studio
-
     return render_edit_studio_form unless @dance_studio.update(dance_studio_params)
 
     redirect_to dance_studio_path(@dance_studio), success: 'Account Info Updated!'
   end
 
   def destroy
-    find_dance_studio
     @dance_studio.destroy
     logout('Account Deleted!')
   end
 
-  # current costume assignments for a dance studio
+  # Displays current costume assignments for a dance studio
   # url: /dance_studios/1/current_assignments
   def current_assignments
     @assignments = current_user.current_studio_assignments
@@ -42,14 +36,14 @@ class DanceStudiosController < ApplicationController
     @season = @assignments.first.dance_season unless @assignments.empty?
   end
 
-  # current costumes for a dance studio
+  # Displays the current season costumes for a dance studio
   # url: /dance_studios/1/current_costumes
   def current_costumes
     @costumes = current_user.current_studio_costumes
     @season = Time.now.year
   end
 
-  # currently unassigned costumes for a dance studio
+  # Displays the current season's unassigned costumes for a dance studio
   # url: /dance_studios/1/unassigned_costumes
   def unassigned_costumes
     @costumes = current_user.unassigned_studio_costumes
