67196bcafb2acb49c9f0c6b4d84b956fb42ec96f
jamieberrier <berrier.jamie@gmail.com>
2020-05-11 14:17:05 -0700

    refactor scope method current_dancer_costumes to use with both dancers and dance_studios and refactor find_by_assignment scope method to find_costumes

--- app/models/costume_assignment.rb
+++ app/models/costume_assignment.rb
@@ -4,6 +4,10 @@ class CostumeAssignment < ApplicationRecord
   # genre, hair_accessory, shoe, tight
   validates :dancer_id, :costume_size, :costume_condition, :dance_season, :song_name, presence: true
 
-  # Gets current costume assignments for a dancer
-  scope :current_dancer_costumes, lambda { |dancer| where(["dancer_id = '%s' and dance_season = '%s'", dancer.id, Time.now.year]) }
+  # Gets current costume assignments for a dancer / dance studio
+  scope :current_assignments, -> { where(dance_season: Time.now.year) }
+
+  # Gets the costumes to display
+  # dance_studios/:id/current_assignments & dance_studios/:id/current_costumes
+  scope :find_costumes, -> { group(:costume_id).collect { |x| Costume.find(x.costume_id) } }
 end
