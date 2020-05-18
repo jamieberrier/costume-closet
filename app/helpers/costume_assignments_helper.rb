064f5356d5941700836b9c6fdc809d5bd33fe9bf
jamieberrier <berrier.jamie@gmail.com>
2020-05-18 11:33:21 -0700

    move method to application controller

--- app/helpers/costume_assignments_helper.rb
+++ app/helpers/costume_assignments_helper.rb
@@ -1,10 +1,2 @@
 module CostumeAssignmentsHelper
-  # Owner
-  # nested, params[:dance_studio_id] -> dance studio id
-  # dancers -- new index current dancers
-  # costumes -- new create index
-  # costume assignments -- index
-  def require_dance_studio_owner
-    redirect_to root_path(message: 'Only studio owner can access') unless owner? && current_user.id == params[:dance_studio_id].to_i
-  end
 end
