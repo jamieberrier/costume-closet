1974e249e4b0b23809557c3665f7d73597c9e3c6
jamieberrier <berrier.jamie@gmail.com>
2020-05-18 15:43:37 -0700

    move require_studio_ownership beore_action to application controller

--- app/controllers/dance_studios_controller.rb
+++ app/controllers/dance_studios_controller.rb
@@ -51,13 +51,6 @@ class DanceStudiosController < ApplicationController
 
   private
 
-  # Studio
-  # unnested, params[:id] -> dance studio id
-  # dance_studios -- show edit update destroy current_assignments current_costumes unassigned_costumes
-  def require_studio_ownership
-    redirect_to root_path(message: 'Only dance studio owner can access') unless owner? && current_user.id == params[:id].to_i
-  end
-
   def find_dance_studio
     @dance_studio = DanceStudio.find(params[:id])
   end
