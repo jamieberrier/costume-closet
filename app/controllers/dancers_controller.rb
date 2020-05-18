6d401f4f885100e37b5217c7e65d0692db0feca0
jamieberrier <berrier.jamie@gmail.com>
2020-05-18 11:37:26 -0700

    move methods from dancers helper to private in dancers controller

--- app/controllers/dancers_controller.rb
+++ app/controllers/dancers_controller.rb
@@ -86,6 +86,29 @@ class DancersController < ApplicationController
 
   private
 
+  # Studio & Dancer: params[:id] -> dancer id
+  # dancers -- show edit update destroy dancer_assignments current_assignments
+  def require_studio_dancer
+    if owner? # check if dancer belongs to studio
+      redirect_to root_path(message: "Only the dancer's studio can access") unless current_user.dancers.include?(find_dancer)
+    else # check if dancer is current user
+      redirect_to root_path(message: 'Denied access') unless current_user.id == params[:id].to_i
+    end
+  end
+
+  def set_dancer
+    @dancer = Dancer.find(params[:id])
+  end
+
+  ## create action helpers
+  def redirect_studio_if_saved
+    redirect_to_dance_studio_page('Dancer Added!') if @saved && owner?
+  end
+
+  def log_in_dancer_if_saved
+    log_in(@dancer, 'Successfully Registered!') if @saved
+  end
+
   def dancer_params
     params.require(:dancer).permit(:dance_studio_id, :first_name, :last_name, :email, :password, :password_confirmation)
   end
--- app/helpers/dancers_helper.rb
+++ app/helpers/dancers_helper.rb
@@ -1,24 +1,2 @@
 module DancersHelper
-  # Studio & Dancer: params[:id] -> dancer id
-  # dancers -- show edit update destroy dancer_assignments current_assignments
-  def require_studio_dancer
-    if owner? # check if dancer belongs to studio
-      redirect_to root_path(message: "Only the dancer's studio can access") unless current_user.dancers.include?(find_dancer)
-    else # check if dancer is current user
-      redirect_to root_path(message: 'Denied access') unless current_user.id == params[:id].to_i
-    end
-  end
-
-  def set_dancer
-    @dancer = Dancer.find(params[:id])
-  end
-
-  ## create action helpers
-  def redirect_studio_if_saved
-    redirect_to_dance_studio_page('Dancer Added!') if @saved && owner?
-  end
-
-  def log_in_dancer_if_saved
-    log_in(@dancer, 'Successfully Registered!') if @saved
-  end
 end
