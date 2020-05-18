f40960f4104bf3ecfd7697405bade0e12b3e6c96
jamieberrier <berrier.jamie@gmail.com>
2020-05-18 11:32:29 -0700

    move methods to dance studios controller

--- app/helpers/dance_studios_helper.rb
+++ app/helpers/dance_studios_helper.rb
@@ -1,16 +1,2 @@
 module DanceStudiosHelper
-  # Studio
-  # unnested, params[:id] -> dance studio id
-  # dance_studios -- show edit update destroy current_assignments current_costumes unassigned_costumes
-  def require_studio_ownership
-    redirect_to root_path(message: 'Only dance studio owner can access') unless owner? && current_user.id == params[:id].to_i
-  end
-
-  def find_dance_studio
-    @dance_studio = DanceStudio.find(params[:id])
-  end
-
-  def redirect_to_dance_studio_page(message)
-    redirect_to dance_studio_path(current_user), success: message
-  end
 end
