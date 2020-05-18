7e56a47d587509b727aeed9d23eb6b77802b34ab
jamieberrier <berrier.jamie@gmail.com>
2020-05-18 13:31:36 -0700

    delete commented out code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,6 @@ class ApplicationController < ActionController::Base
 
   add_flash_types :success, :danger, :warning, :info
 
-  #include SessionsHelper
-  #include RegistrationsHelper
-  #include DancersHelper
-  #include DanceStudiosHelper
-  #include CostumesHelper
-  #include CostumeAssignmentsHelper
-
   before_action :require_logged_in, except: :home
 
   def home
