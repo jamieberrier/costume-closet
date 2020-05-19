68be41401e01e9965104373ceb648f40edc194c1
jamieberrier <berrier.jamie@gmail.com>
2020-05-19 08:27:50 -0700

    create view helpers register_with_email_button, register_via_google_button, and sign_in_button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def register_with_email_button(user_type)
+    link_to 'Register with email', register_path(user_type: user_type), class: 'heading is-size-6 has-text-white'
+  end
+
+  def register_via_google_button(user_type)
+    link_to 'Register via Google', google_omniauth_path(user_type: user_type), class: 'heading is-size-6 has-text-white'
+  end
+
+  def sign_in_button(user_type)
+    link_to "Sign In As A #{user_type.gsub(/_/, ' ')}", login_path(user_type: user_type), class: 'button is-primary is-outlined'
+  end
 end
