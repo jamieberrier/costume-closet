ba8e9bc91fc7a6d674eacbb8f4ad22efc043ce1a
jamieberrier <berrier.jamie@gmail.com>
2020-05-18 18:26:17 -0700

    create view helpers

--- app/helpers/dancers_helper.rb
+++ app/helpers/dancers_helper.rb
@@ -1,2 +1,17 @@
 module DancersHelper
+  def picture_link_to_costume_path(costume)
+    link_to image_tag(costume.picture, width: 100), costume_path(costume)
+  end
+
+  def onepiece_link_to_costume_path(costume)
+    link_to costume.onepiece_description, costume_path(costume)
+  end
+
+  def twopiece_link_to_costume_path(costume)
+    link_to costume.twopiece_description, costume_path(costume)
+  end
+
+  def assignment_costume(assignment)
+    Costume.find(assignment.costume_id)
+  end
 end
