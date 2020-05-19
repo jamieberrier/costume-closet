a2c04491c44e1bf0cee43deb76eb404a3490e9a0
jamieberrier <berrier.jamie@gmail.com>
2020-05-19 07:34:10 -0700

    create view helpers song_name_link and dancer_name_link

--- app/helpers/costume_assignments_helper.rb
+++ app/helpers/costume_assignments_helper.rb
@@ -1,2 +1,11 @@
 module CostumeAssignmentsHelper
+  def song_name_link(assignment)
+    link_to_if owner?, assignment.song_name, season_assignments_path(assignment.costume_id, season: assignment.dance_season) do |name|
+      link_to name, costume_path(assignment.costume_id)
+    end
+  end
+
+  def dancer_name_link(assignment)
+    link_to full_name(Dancer.find(assignment.dancer_id)), dancer_costumes_path(assignment.dancer_id, dancer_id: assignment.dancer_id) 
+  end
 end
