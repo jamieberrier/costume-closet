6df250044437067773b1fbcf0d67d27ca2841df1
jamieberrier <berrier.jamie@gmail.com>
2020-05-19 08:36:09 -0700

    create view helpers sign_in_via_google and cancel_button

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+  def sign_in_via_google(user_type)
+    link_to 'Sign In with Google', google_omniauth_path(user_type: user_type), method: :get, class: 'has-text-primary'
+  end
+
+  def cancel_button(path)
+    link_to 'Cancel', path, class: 'button is-dark'
+  end
 end
