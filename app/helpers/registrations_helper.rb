dd898c6557f5919c834a96a6948adcff6f891eae
jamieberrier <berrier.jamie@gmail.com>
2020-05-19 09:42:20 -0700

    create view helpers any_validation_errors, error_count, and any_field_errors

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,5 +1,18 @@
 module RegistrationsHelper
-  def field_error_explaination(user, field)
-    user.errors.full_messages_for(field).to_sentence
+  # form_for validation errors
+  def any_validation_errors(user_type)
+    user_type.errors.any?
+  end
+
+  def error_count(user_type)
+    pluralize(user_type.errors.count, 'error')
+  end
+
+  def any_field_errors(user_type, field)
+    user_type.errors[field].any?
+  end
+
+  def field_error_explaination(user_type, field)
+    user_type.errors.full_messages_for(field).to_sentence.downcase
   end
 end
