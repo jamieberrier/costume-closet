f1d1f3e5e856675fb9bd6a02ae5e98096cca2f82
jamieberrier <berrier.jamie@gmail.com>
2020-05-19 07:34:54 -0700

    create view helpers set_costume_path and show_costume

--- app/helpers/costumes_helper.rb
+++ app/helpers/costumes_helper.rb
@@ -3,4 +3,18 @@ module CostumesHelper
   def unassigned?
     current_user.unassigned_studio_costumes.include?(@costume)
   end
+
+  def set_costume_path(costume, season)
+    season ? costume_path(costume, season: season) : costume_path(costume)
+  end
+
+  def show_costume(costume, path)
+    if !costume.picture.blank?
+      link_to image_tag(costume.picture, width: 100), path
+    elsif !costume.onepiece_description.blank?
+      link_to costume.onepiece_description, path
+    else
+      link_to costume.twopiece_description, path
+    end
+  end
 end
