b4b15a777148ea4329a33d95f8b91078f9daeb37
jamieberrier <berrier.jamie@gmail.com>
2020-05-20 10:12:28 -0700

    change argument to object_type

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,18 +1,18 @@
 module RegistrationsHelper
   # form_for validation errors
-  def any_validation_errors?(user_type)
-    user_type.errors.any?
+  def any_validation_errors?(object_type)
+    object_type.errors.any?
   end
 
-  def error_count(user_type)
-    pluralize(user_type.errors.count, 'error')
+  def error_count(object_type)
+    pluralize(object_type.errors.count, 'error')
   end
 
-  def any_field_errors?(user_type, field)
-    user_type.errors[field].any?
+  def any_field_errors?(object_type, field)
+    object_type.errors[field].any?
   end
 
-  def field_error_explaination(user_type, field)
-    user_type.errors.full_messages_for(field).to_sentence.downcase
+  def field_error_explaination(object_type, field)
+    object_type.errors.full_messages_for(field).to_sentence.downcase
   end
 end
