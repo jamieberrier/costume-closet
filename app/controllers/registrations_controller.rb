234f535b63a93b3b7e6503d096ad2a7a9dfe8cc1
jamieberrier <berrier.jamie@gmail.com>
2020-05-20 18:02:53 -0700

    delete unused method

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,10 +12,6 @@ class RegistrationsController < ApplicationController
     params[:user_type] == 'dance_studio'
   end
 
-  #def signing_up_as_dancer?
-    #params[:user_type] == 'dancer'
-  #end
-
   def create_empty_user
     signing_up_as_dance_studio? ? @dance_studio = DanceStudio.new : @dancer = Dancer.new
   end
