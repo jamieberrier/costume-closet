4492645cef5009799ab0c2cfcc6b5a987d6e0ba2
jamieberrier <berrier.jamie@gmail.com>
2020-05-21 10:40:32 -0700

    add method comments

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,7 @@
 class RegistrationsController < ApplicationController
   skip_before_action :require_logged_in
 
+  # GET /register
   def new
     redirect_if_logged_in('You are already logged in')
     create_empty_user
@@ -8,10 +9,12 @@ class RegistrationsController < ApplicationController
 
   private
 
+  # Returns true if user_type is dance_studio
   def signing_up_as_dance_studio?
     params[:user_type] == 'dance_studio'
   end
 
+  # Creates instance based on user_type
   def create_empty_user
     signing_up_as_dance_studio? ? @dance_studio = DanceStudio.new : @dancer = Dancer.new
   end
